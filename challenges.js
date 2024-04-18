dd67a17b117d53e9701b1ce47e1434e06bd07429
kiibra <kibra99@gmail.com>
2024-04-17 23:12:18 -0400

    complete exercise 3

--- challenges.js
+++ challenges.js
@@ -87,10 +87,14 @@ sumNumbers([5, 10]) //=> 15
 sumNumbers([2, 10, -5]) //=> 7
 sumNumbers([]) //=> 0
 -----------------------------------------------------------------------------*/
-// Your solution for 03-sumNumbers here:
-
-
-
+// Your solution for 03-sumNumbers here
+function sumNumbers (nums){
+  let sum = 0
+  for(let i= 0; i < nums.length; i++){
+    sum += nums[i];
+  }
+  return sum
+}
 
 
 /*-----------------------------------------------------------------------------
